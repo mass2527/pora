3e658dd0093eddc171fdb2204c81b17783d80ccd
Dongho Kim <dongre2527@gmail.com>
2024-01-30 14:20:18 +0900

    Add EmptyPlaceholder

---
+++ src/components/empty-placeholder.tsx
@@ -0,0 +1,24 @@
+import { ReactNode } from "react";
+
+export function EmptyPlaceholder({
+  icon,
+  title,
+  description,
+  action,
+}: {
+  icon?: ReactNode;
+  title: string;
+  description: string;
+  action?: ReactNode;
+}) {
+  return (
+    <div className="flex h-[450px] shrink-0 items-center justify-center rounded-md border border-dashed">
+      <div className="mx-auto flex max-w-[420px] flex-col items-center justify-center text-center">
+        {icon}
+        <h3 className="mt-4 text-lg font-semibold">{title}</h3>
+        <p className="mb-4 mt-2 text-sm text-muted-foreground">{description}</p>
+        {action}
+      </div>
+    </div>
+  );
+}
