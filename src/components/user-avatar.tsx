81d0cb86ae49ab0e390bb6279a64797a8d32c670
Dongho Kim <dongre2527@gmail.com>
2024-02-04 17:35:27 +0900

    Update user avatar props

--- src/components/user-avatar.tsx
+++ src/components/user-avatar.tsx
@@ -2,11 +2,17 @@ import { User } from "@prisma/client";
 import React from "react";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 
-export default function UserAvatar({ user }: { user: User }) {
+export default function UserAvatar({
+  user,
+  className,
+}: {
+  user: Pick<User, "name" | "image">;
+  className?: string;
+}) {
   const lastName = user.name?.split(" ")[1];
 
   return (
-    <Avatar>
+    <Avatar className={className}>
       <AvatarImage src={user.image ?? undefined} />
       <AvatarFallback>{lastName}</AvatarFallback>
     </Avatar>
