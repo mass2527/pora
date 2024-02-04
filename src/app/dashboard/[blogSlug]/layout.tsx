e8754e8fb2d2cca7f59a3e7963a88c512713c396
Dongho Kim <dongre2527@gmail.com>
2024-02-04 17:35:03 +0900

    Update layout

--- src/app/dashboard/[blogSlug]/layout.tsx
+++ src/app/dashboard/[blogSlug]/layout.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import React, { ReactNode } from "react";
 import { buttonVariants } from "~/components/ui/button";
+import { cn } from "~/lib/utils";
 
 export default function BlogLayout({
   params,
@@ -13,11 +14,11 @@ export default function BlogLayout({
     <div>
       <div className="border-b">
         <nav className="px-4">
-          <ul className="flex">
+          <ul className="flex gap-4">
             <li>
               <Link
                 href={`/dashboard/${params.blogSlug}`}
-                className={buttonVariants({ variant: "link" })}
+                className={cn(buttonVariants({ variant: "link" }), "px-0")}
               >
                 아티클
               </Link>
@@ -25,7 +26,7 @@ export default function BlogLayout({
             <li>
               <Link
                 href={`/dashboard/${params.blogSlug}/categories`}
-                className={buttonVariants({ variant: "link" })}
+                className={cn(buttonVariants({ variant: "link" }), "px-0")}
               >
                 카테고리
               </Link>
