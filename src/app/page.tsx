d472211e07ecedb863d133fe1a27320bb9535e97
Dongho Kim <dongre2527@gmail.com>
2024-02-08 17:27:42 +0900

    Update landing page link style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
 import Link from "next/link";
+import { buttonVariants } from "~/components/ui/button";
 
 export default function Home() {
   return (
-    <div>
-      <Link href="/dashboard">Get started</Link>
+    <div className="grid place-items-center min-h-screen">
+      <div>
+        <Link href="/dashboard" className={buttonVariants()}>
+          블로그 생성
+        </Link>
+      </div>
     </div>
   );
 }
