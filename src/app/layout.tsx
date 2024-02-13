5ff7e9273f14db82994b1584c7e591fca257ed5f
Dongho Kim <dongre2527@gmail.com>
2024-02-13 21:55:13 +0900

    Add verification codes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,12 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Pora",
   description: "블로그를 생성하고 생각과 경험을 공유하세요.",
+  verification: {
+    google: "UrAbseg9KUEcpygfHffZg2n-ZY5nIoqY3JAHT0jLzqU",
+    other: {
+      "naver-site-verification": "c24772a72d9ca2f42398690c7c50f2df5a9ab4ab",
+    },
+  },
 };
 
 export default function RootLayout({
