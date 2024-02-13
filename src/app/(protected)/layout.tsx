a25b6c8101ad82bb6fa519ff050561f69ada2478
Dongho Kim <dongre2527@gmail.com>
2024-02-13 14:02:19 +0900

    Add logo

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -7,6 +7,7 @@ import prisma from "~/lib/prisma";
 import BlogNav from "./dashboard/blog-nav";
 import RootNav from "./root-nav";
 import UserAccountNav from "./dashboard/user-account-nav";
+import Image from "next/image";
 
 export default async function ProtectedLayout({
   children,
@@ -28,11 +29,11 @@ export default async function ProtectedLayout({
     <div>
       <div className="flex justify-between items-center p-4">
         <div className="flex items-center gap-4 h-8">
-          <Link
-            href="/dashboard"
-            className="text-xl font-semibold tracking-tight"
-          >
-            Pora
+          <Link href="/dashboard">
+            <div className="flex items-center gap-2">
+              <Image src="/pora.svg" alt="Pora Logo" width={24} height={24} />
+              <span className="text-xl font-semibold tracking-tight">Pora</span>
+            </div>
           </Link>
           <BlogNav blogs={blogs} />
         </div>
