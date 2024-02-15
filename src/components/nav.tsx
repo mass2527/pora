a30da7c74c8a88b8fa0006a13b2cfe1b5e011015
Dongho Kim <dongre2527@gmail.com>
2024-02-15 14:37:13 +0900

    Add nav component

---
+++ src/components/nav.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import NavLink from "./nav-link";
+
+export interface NavProps {
+  links: { href: string; name: string }[];
+}
+
+export default function Nav({ links }: NavProps) {
+  return (
+    <nav className="px-4">
+      <ul className="flex">
+        {links.map((link) => {
+          return (
+            <li key={link.href}>
+              <NavLink href={link.href}>{link.name}</NavLink>
+            </li>
+          );
+        })}
+      </ul>
+    </nav>
+  );
+}
