bd3564cabedfffbe6851a13a5087891ffcc5eb6b
Dongho Kim <dongre2527@gmail.com>
2024-02-16 14:18:25 +0900

    Update landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,34 @@
+import Image from "next/image";
 import Link from "next/link";
 import { buttonVariants } from "~/components/ui/button";
 
 export default function Home() {
   return (
-    <div className="grid place-items-center min-h-screen">
-      <div>
-        <Link href="/dashboard" className={buttonVariants()}>
-          블로그 생성
-        </Link>
+    <div className="min-h-screen">
+      <header className="flex justify-between items-center p-4">
+        <div className="flex items-center gap-2">
+          <Image src="/pora.svg" alt="Pora 로고" width={24} height={24} />
+          <span className="text-xl font-semibold tracking-tight">Pora</span>
+        </div>
+      </header>
+
+      <div className="grid place-items-center h-[calc(100vh-60px)] p-4">
+        <div className="flex flex-col items-center gap-8">
+          <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl text-center">
+            블로그를 생성하고 생각과 경험을 공유하세요.
+          </h1>
+
+          <Link href="/dashboard" className={buttonVariants()}>
+            시작하기
+          </Link>
+          <Image
+            className="border rounded-md"
+            src="/introduction.png"
+            alt="Pora "
+            width={720}
+            height={500}
+          />
+        </div>
       </div>
     </div>
   );
