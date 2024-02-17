177a338d8f919f01f46da9de02ac6680f2a8d35c
Dongho Kim <dongre2527@gmail.com>
2024-02-17 17:51:29 +0900

    Change the sitemap to be dynamically generated

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -2,6 +2,8 @@ import { MetadataRoute } from "next";
 import prisma from "~/lib/prisma";
 import { invariant } from "~/lib/utils";
 
+export const dynamic = "force-dynamic";
+
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const baseUrl = process.env.NEXTAUTH_URL;
   invariant(baseUrl);
