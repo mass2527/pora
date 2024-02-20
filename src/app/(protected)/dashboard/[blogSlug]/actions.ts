e76597a3fc645b13e02ced3ebe01eb87d74cad65
Dongho Kim <dongre2527@gmail.com>
2024-02-20 18:06:20 +0900

    Replace `createBlogArticle` to server action

--- src/app/(protected)/dashboard/[blogSlug]/actions.ts
+++ src/app/(protected)/dashboard/[blogSlug]/actions.ts
@@ -5,6 +5,10 @@ import { ServerActionResponse } from "./categories/actions";
 import { getUser } from "~/lib/auth";
 import prisma from "~/lib/prisma";
 import { revalidatePath } from "next/cache";
+import { createArticleSchema } from "~/lib/validations/article";
+import { z } from "zod";
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
+import { PRISMA_ERROR_CODES } from "~/lib/constants";
 
 export async function deleteArticle(
   articleId: string,
@@ -70,6 +74,59 @@ export async function deleteBlog(
       data: blog,
     };
   } catch (error) {
+    return {
+      status: "failure",
+      error: {
+        message: "Unknown",
+        status: 500,
+      },
+    };
+  }
+}
+
+export async function createArticle(
+  blogId: string,
+  values: z.infer<typeof createArticleSchema>,
+  path: string
+): Promise<ServerActionResponse<Article>> {
+  try {
+    const user = await getUser();
+    if (!user) {
+      return {
+        status: "failure",
+        error: {
+          message: "Unauthorized",
+          status: 401,
+        },
+      };
+    }
+
+    const article = await prisma.article.create({
+      data: {
+        ...values,
+        blogId,
+      },
+    });
+    revalidatePath(path);
+
+    return {
+      status: "success",
+      data: article,
+    };
+  } catch (error) {
+    if (error instanceof PrismaClientKnownRequestError) {
+      if (error.code === PRISMA_ERROR_CODES.UNIQUE_CONSTRAINT_FAILED) {
+        return {
+          status: "failure",
+          error: {
+            data: error.meta,
+            status: 409,
+            message: "Unique constraint failed",
+          },
+        };
+      }
+    }
+
     return {
       status: "failure",
       error: {
--- src/app/(protected)/dashboard/[blogSlug]/create-blog-article-button.tsx
+++ src/app/(protected)/dashboard/[blogSlug]/create-blog-article-button.tsx
@@ -1,18 +1,20 @@
 "use client";
 
 import { ArticleStatus, Blog } from "@prisma/client";
-import { useRouter } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 import React, { useEffect } from "react";
 import { Button } from "~/components/ui/button";
 import { Loading } from "~/components/ui/loading";
-import { handleError } from "~/lib/errors";
+import { handleError, throwServerError } from "~/lib/errors";
 import { useAtom } from "jotai";
-import { createBlogArticle } from "~/services/blog/article";
+
 import { createBlogArticleLoadingAtom } from "./create-blog-article-loading-atom";
+import { createArticle } from "./actions";
 
 export default function CreateBlogArticleButton({ blog }: { blog: Blog }) {
   const router = useRouter();
   const [isLoading, setIsLoading] = useAtom(createBlogArticleLoadingAtom);
+  const pathname = usePathname();
 
   useEffect(() => {
     return () => {
@@ -26,16 +28,25 @@ export default function CreateBlogArticleButton({ blog }: { blog: Blog }) {
       onClick={async () => {
         try {
           setIsLoading(true);
-          const article = await createBlogArticle(blog.id, {
-            slug: crypto.randomUUID(),
-            title: "",
-            draftTitle: "",
-            description: "",
-            jsonContent: JSON.stringify({}),
-            draftJsonContent: JSON.stringify({}),
-            htmlContent: "",
-            status: ArticleStatus.WRITING,
-          });
+          const response = await createArticle(
+            blog.id,
+            {
+              slug: crypto.randomUUID(),
+              title: "",
+              draftTitle: "",
+              description: "",
+              jsonContent: JSON.stringify({}),
+              draftJsonContent: JSON.stringify({}),
+              htmlContent: "",
+              status: ArticleStatus.WRITING,
+            },
+            pathname
+          );
+          if (response.status === "failure") {
+            throwServerError(response);
+          }
+
+          const article = response.data;
           router.push(`/dashboard/${blog.slug}/${article.slug}/edit`);
         } catch (error) {
           setIsLoading(false);
