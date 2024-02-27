6a3c0d630b17d01a0c88c89ab310b08acee8a6cf
Dongho Kim <dongre2527@gmail.com>
2024-02-27 18:03:26 +0900

    Refactor blog articles page

---
+++ src/app/(protected)/dashboard/[blogSlug]/get-blog.ts
@@ -0,0 +1,26 @@
+import { cache } from "react";
+import prisma from "~/lib/prisma";
+import { renderNotFoundIfNullable } from "~/lib/promise";
+
+const getBlogOrNull = cache(async (userId: string, blogSlug: string) => {
+  const blog = await prisma.blog.findUnique({
+    where: {
+      slug: blogSlug,
+      userId: userId,
+    },
+    include: {
+      articles: {
+        include: {
+          blog: true,
+          category: true,
+        },
+      },
+    },
+  });
+
+  return blog;
+});
+
+export function getBlog(userId: string, blogSlug: string) {
+  return renderNotFoundIfNullable(getBlogOrNull(userId, blogSlug));
+}
--- src/app/(protected)/dashboard/[blogSlug]/page.tsx
+++ src/app/(protected)/dashboard/[blogSlug]/page.tsx
@@ -1,45 +1,20 @@
 import { ArticleStatus } from "@prisma/client";
-import { notFound } from "next/navigation";
 import { Button } from "~/components/ui/button";
-import prisma from "~/lib/prisma";
 import CreateBlogArticleButton from "./create-blog-article-button";
 import { getUser } from "~/lib/auth";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "~/components/ui/tabs";
-import { Suspense, cache } from "react";
+import { Suspense } from "react";
 import Await from "~/components/await";
 import { Skeleton } from "~/components/ui/skeleton";
 import { BlogArticlesTable } from "./blog-articles-table";
-import { ifNullable } from "~/lib/promise";
 import { assertAuthenticated } from "~/lib/asserts";
+import { getBlog } from "./get-blog";
 
-const articleStatuses = {
+const ARTICLE_STATUSES = {
   PUBLISHED: "발행됨",
   WRITING: "작성중",
   HIDDEN: "숨겨짐",
-};
-
-const getBlogOrNull = cache(async (userId: string, blogSlug: string) => {
-  const blog = await prisma.blog.findUnique({
-    where: {
-      slug: blogSlug,
-      userId: userId,
-    },
-    include: {
-      articles: {
-        include: {
-          blog: true,
-          category: true,
-        },
-      },
-    },
-  });
-
-  return blog;
-});
-
-function getBlog(userId: string, blogSlug: string) {
-  return ifNullable(getBlogOrNull(userId, blogSlug), notFound);
-}
+} as const;
 
 export default async function BlogArticlesPage({
   params,
@@ -63,10 +38,10 @@ export default async function BlogArticlesPage({
       <Tabs defaultValue={ArticleStatus.WRITING}>
         <TabsList>
           <TabsTrigger value={ArticleStatus.WRITING}>
-            {articleStatuses[ArticleStatus.WRITING]}
+            {ARTICLE_STATUSES[ArticleStatus.WRITING]}
           </TabsTrigger>
           <TabsTrigger value={ArticleStatus.PUBLISHED}>
-            {articleStatuses[ArticleStatus.PUBLISHED]}
+            {ARTICLE_STATUSES[ArticleStatus.PUBLISHED]}
           </TabsTrigger>
         </TabsList>
         <TabsContent value={ArticleStatus.WRITING}>
