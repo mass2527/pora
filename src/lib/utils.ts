509319b47cc3393d8ca0e2309ff2399ccd808fdd
Dongho Kim <dongre2527@gmail.com>
2024-02-27 16:51:01 +0900

    Remove unused function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -24,10 +24,6 @@ export function formatDate(
   return new Intl.DateTimeFormat(locales, options).format(date);
 }
 
-export function assertNever(x: never): never {
-  throw new Error("Unexpected object: " + x);
-}
-
 export function sleep(ms: number) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
