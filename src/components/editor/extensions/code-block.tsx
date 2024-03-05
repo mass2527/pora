519ceacb6c9ad32b06f7432c11b67af143dd3e14
Dongho Kim <dongre2527@gmail.com>
2024-03-05 15:43:20 +0900

    Fix grammar bug

--- src/components/editor/extensions/code-block.tsx
+++ src/components/editor/extensions/code-block.tsx
@@ -9,7 +9,6 @@ import {
   SelectTrigger,
   SelectValue,
 } from "~/components/ui/select";
-import { assert } from "~/lib/utils";
 
 interface CodeBlockProps {
   node: {
@@ -34,8 +33,7 @@ export default function CodeBlock({
   updateAttributes,
   extension,
 }: CodeBlockProps) {
-  const grammars =
-    extension.options.starryNight.scopes() as (keyof typeof COMMON_GRAMMAR_LABELS)[];
+  const grammars = extension.options.starryNight.scopes();
 
   return (
     <NodeViewWrapper className="relative">
@@ -45,18 +43,24 @@ export default function CodeBlock({
           defaultValue={language ?? extension.options.defaultLanguage}
         >
           <SelectTrigger>
-            <SelectValue placeholder="언어 선택" />
+            <SelectValue />
           </SelectTrigger>
 
           <SelectContent>
             {grammars.map((grammar) => {
-              const label = COMMON_GRAMMAR_LABELS[grammar];
+              if (!(grammar in GRAMMARS)) {
+                throw new Error(
+                  `Unregistered grammar: ${grammar}. Please add grammar to \`GRAMMARS\``
+                );
+              }
+
+              const { label, name } = GRAMMARS[grammar as CommonGrammar];
               if (!label) {
                 return null;
               }
 
               return (
-                <SelectItem key={grammar} value={getGrammarName(grammar)}>
+                <SelectItem key={grammar} value={name}>
                   {label}
                 </SelectItem>
               );
@@ -71,62 +75,153 @@ export default function CodeBlock({
   );
 }
 
+type CommonGrammar = keyof typeof GRAMMARS;
+
 // https://github.com/wooorm/starry-night?tab=readme-ov-file#languages
 // https://github.com/wooorm/starry-night?tab=readme-ov-file#common
-const COMMON_GRAMMAR_LABELS = {
-  "source.c": "C",
-  "source.c++": "C++",
-  "source.c.platform": undefined,
-  "source.cs": "C#",
-  "source.css": "CSS",
-  "source.css.less": "Less",
-  "source.css.scss": "Scss",
-  "source.diff": "Diff",
-  "source.go": "Go",
-  "source.graphql": "GraphQL",
-  "source.ini": "INI",
-  "source.java": "Java",
-  "source.js": "JavaScript",
-  "source.json": "JSON",
-  "source.kotlin": "Kotlin",
-  "source.lua": "Lua",
-  "source.makefile": "Makefile",
-  "source.objc": "Objective-C",
-  "source.objc.platform": undefined,
-  "source.perl": "Perl",
-  "source.python": "Python",
-  "source.r": "R",
-  "source.ruby": "Ruby",
-  "source.rust": "Rust",
-  "source.shell": "Shell",
-  "source.sql": "SQL",
-  "source.swift": "Swift",
-  "source.ts": "TypeScript",
-  "source.vbnet": "VB.Net",
-  "source.yaml": "YAML",
-  "text.html.basic": "HTML",
-  "text.html.php": "PHP",
-  "text.md": "Markdown",
-  "text.xml": "XML",
-  "text.xml.svg": undefined,
-} as const;
-
 // https://github.com/wooorm/starry-night?tab=readme-ov-file#starrynightflagtoscopeflag
-function getGrammarName(grammar: keyof typeof COMMON_GRAMMAR_LABELS) {
-  const isSource = grammar.startsWith("source");
-  if (isSource) {
-    const words = grammar.split(".");
-    const grammarName = words[words.length - 1];
-    assert(grammarName);
-    return grammarName;
-  }
-
-  const isText = grammar.startsWith("text");
-  if (isText) {
-    const [_, grammarName] = grammar.split(".");
-    assert(grammarName);
-    return grammarName;
-  }
-
-  throw new Error(`Invalid case`);
-}
+const COMMON_GRAMMARS = {
+  "source.c": {
+    name: "c",
+    label: "C",
+  },
+  "source.c++": {
+    name: "c++",
+    label: "C++",
+  },
+  "source.c.platform": {
+    name: undefined,
+    label: undefined,
+  },
+  "source.cs": {
+    name: "c#",
+    label: "C#",
+  },
+  "source.css": {
+    name: "css",
+    label: "CSS",
+  },
+  "source.css.less": {
+    name: "less",
+    label: "Less",
+  },
+  "source.css.scss": {
+    name: "scss",
+    label: "Scss",
+  },
+  "source.diff": {
+    name: "diff",
+    label: "Diff",
+  },
+  "source.go": {
+    name: "go",
+    label: "Go",
+  },
+  "source.graphql": {
+    name: "graphql",
+    label: "GraphQL",
+  },
+  "source.ini": {
+    name: "ini",
+    label: "INI",
+  },
+  "source.java": {
+    name: "java",
+    label: "Java",
+  },
+  "source.js": {
+    name: "js",
+    label: "JavaScript",
+  },
+  "source.json": {
+    name: "json",
+    label: "JSON",
+  },
+  "source.kotlin": {
+    name: "kotlin",
+    label: "Kotlin",
+  },
+  "source.lua": {
+    name: "lua",
+    label: "Lua",
+  },
+  "source.makefile": {
+    name: "makefile",
+    label: "Makefile",
+  },
+  "source.objc": {
+    name: "objc",
+    label: "Objective-C",
+  },
+  "source.objc.platform": {
+    name: undefined,
+    label: undefined,
+  },
+  "source.perl": {
+    name: "perl",
+    label: "Perl",
+  },
+  "source.python": {
+    name: "python",
+    label: "Python",
+  },
+  "source.r": {
+    name: "r",
+    label: "R",
+  },
+  "source.ruby": {
+    name: "ruby",
+    label: "Ruby",
+  },
+  "source.rust": {
+    name: "rust",
+    label: "Rust",
+  },
+  "source.shell": {
+    name: "shell",
+    label: "Shell",
+  },
+  "source.sql": {
+    name: "sql",
+    label: "SQL",
+  },
+  "source.swift": {
+    name: "swift",
+    label: "Swift",
+  },
+  "source.ts": {
+    name: "ts",
+    label: "TypeScript",
+  },
+  "source.vbnet": {
+    name: "vbnet",
+    label: "VB.Net",
+  },
+  "source.yaml": {
+    name: "yaml",
+    label: "YAML",
+  },
+  "text.html.basic": {
+    name: "html",
+    label: "HTML",
+  },
+  "text.html.php": {
+    name: "php",
+    label: "PHP",
+  },
+  "text.md": {
+    name: "md",
+    label: "Markdown",
+  },
+  "text.xml": {
+    name: "xml",
+    label: "XML",
+  },
+  "text.xml.svg": {
+    name: "svg",
+    label: "SVG",
+  },
+};
+const GRAMMARS = {
+  ...COMMON_GRAMMARS,
+};
